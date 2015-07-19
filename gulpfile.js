40e56d288ce6874076045019025121561a617421
Blaine Bublitz <blaine@iceddev.com>
2015-07-19 16:39:13 -0700

    publish the scss file with npm package - closes #4

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('js', function(){
 });
 
 gulp.task('css', function(){
-  return gulp.src(['./mfb/src/mfb.css', './mfb/src/mfb.css.map'])
+  return gulp.src(['./mfb/src/mfb.css', './mfb/src/mfb.css.map', './mfb/src/mfb.scss'])
     .pipe(gulp.dest('./'));
 });
 
