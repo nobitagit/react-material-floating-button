0933e58715b6147b3193d7c44c52e6cbb7ee33ee
kozniak <kozniak@gmail.com>
2016-09-02 15:36:21 +0300

    Added disable option to a child button (<ChildButton diabled/>).

--- src/child-button.js
+++ src/child-button.js
@@ -4,14 +4,26 @@ var React = require('react');
 var classnames = require('classnames');
 
 var ChildButton = React.createClass({
+
+  handleOnClick: function handleOnClick() {
+    if(this.props.disabled === true)
+    {
+      return;
+    }
+
+    this.props.onClick();
+  },
+
   render: function(){
     var iconClass = classnames('mfb-component__child-icon', this.props.icon);
-    var className = classnames('mfb-component__button--child', this.props.className);
+    var className = classnames('mfb-component__button--child',
+                               this.props.className,
+                               {"mfb-component__button--disabled": this.props.disabled});
     return (
       <li>
         <a href={this.props.href}
            data-mfb-label={this.props.label}
-           onClick={this.props.onClick}
+           onClick={this.handleOnClick}
            className={className}>
           <i className={iconClass}></i>
         </a>
